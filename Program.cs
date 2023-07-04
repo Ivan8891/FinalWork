b9134ab97c649ecad4363ad4410f22dd1b98b42a
Ivan <crueldog50@gmail.com>
2023-07-05 00:50:29 +1000

    finish

--- Program.cs
+++ Program.cs
@@ -11,5 +11,27 @@
 	}
     return arr;
 }
-
+string[] NewArray (string [] array)
+{
+	int count = 0;
+	for(int i = 0; i < array.Length; i++)
+	{
+		if(array[i].Length <= 3) count++;
+	}
+	string[] newArray = new string[count];
+    int index = 0;
+	for ( int i = 0; i < array.Length; i++)
+	{
+		if(array[i].Length <= 3)
+		{
+		    newArray[index] = array[i];
+            index++;
+		}
+	}
+    return newArray;
+}
+string[] array = GetArray();
+string[] newArray = NewArray(array);
+Console.WriteLine($"[{String.Join(" ", array)}]");
+Console.WriteLine($"[{String.Join(" ", newArray)}]");
 
