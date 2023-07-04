1b346f0a747b0f05978b12057c295eb6b0380de7
Ivan <crueldog50@gmail.com>
2023-07-05 00:10:51 +1000

    GetArray

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿string[] GetArray ()
+{
+    Console.Write("Задайте длину массива: ");
+	int n = Convert.ToInt32(Console.ReadLine());
+	string[] arr = new string[n];
+	Console.WriteLine("Введите значения для множества: ");
+	for (int i = 0; i < arr.Length; i++)
+	{
+		Console.Write($"{i + 1}: ");
+		arr[i] = Console.ReadLine();
+	}
+    return arr;
+}
+
+
